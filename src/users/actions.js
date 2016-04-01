ea40231896c33b742deeb561222f1ea592ea2f10
Jamie Weatherby <jamie.weatherby@gmail.com>
2016-04-01 09:21:02 -0400

    Add validation when adding users

--- src/users/actions.js
+++ src/users/actions.js
@@ -1,9 +1,23 @@
-let lastId = 0;
+let lastUserId = 0;
 export const createUser = (state, username) => {
+
+    let userFound = false, errorMsg = false;
+    if(!username)
+        errorMsg = 'Username cannot be empty';
+
+    state.userlist.map(user => {
+        if(user.username == username)
+            errorMsg = 'That user already exists';
+    });
+
+    if(errorMsg)
+        return Object.assign({}, state, {errorMsg});
+
     return Object.assign({}, state, {
+        errorMsg: '',
         userlist: [
             ...state.userlist,
-            {id: ++lastId, username}
+            {id: ++lastUserId, username}
         ]
     });
 }
--- src/users/components/createuser.jsx
+++ src/users/components/createuser.jsx
@@ -2,6 +2,10 @@ import React from 'react';
 import {bindActionCreators} from 'redux';
 import {connect} from 'react-redux';
 
+const mapStateToProps = (state) => ({
+    errorMsg: state.users.errorMsg
+})
+
 const mapDispatchToProps = (dispatch) => ({
     onCreateUserClick: (username) => dispatch({
         type: 'CREATE_USER', username
@@ -10,18 +14,20 @@ const mapDispatchToProps = (dispatch) => ({
 
 class CreateUser extends React.Component{
     render(){
+        console.log("CreateUserProps", this);
         return (
             <div>
                 <input ref="username" placeholder="Username"/>
                 <button onClick={() => this.props.onCreateUserClick(this.refs.username.value)}>
                     Create!
                 </button>
+                <p>{this.props.errorMsg}</p>
             </div>
         );
     }
 }
 
 export default connect(
-    ()=>({}),
+    mapStateToProps,
     mapDispatchToProps
 )(CreateUser);
