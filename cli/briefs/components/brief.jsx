11eef9e6b2628e3a0e188f991e5a69b8c8e1ef39
Jamie Weatherby <jamie.weatherby@gmail.com>
2016-08-07 18:55:18 -0400

    Add minor styling for brief resources

--- cli/briefs/components/brief.jsx
+++ cli/briefs/components/brief.jsx
@@ -5,6 +5,9 @@ import {Col, Tabs, Tab} from 'react-bootstrap';
 
 import Chat from '../../chat/components/chat';
 import CreateChat from '../../chat/components/create';
+
+import sass from '../css/brief.scss';
+
 // the two xs={6} will be replaced with a chat and
 // resource tabs respectively
 
@@ -12,18 +15,23 @@ class Brief extends React.Component {
 
     render(){
         return (
-            <div className="clearfix">
+            <div className="clearfix brief">
                 <Col xs={8}>
                     <h2> Brief title </h2>
                     <Chat/>
                     <CreateChat/>
                 </Col>
                 <Col xs={4}>
-                    <Tabs defaultActiveKey="4" id="stupidtabs" className="pull-right">
-                        <Tab eventKey="1" title="Notes"> </Tab>
+                    <Tabs bsStyle="pills"
+                        defaultActiveKey="1"
+                        id="stupidtabs"
+                        className="resources">
+                        <Tab eventKey="1" title="Photos">
+                            Some photos will go here
+                        </Tab>
                         <Tab eventKey="2" title="Links"> </Tab>
                         <Tab eventKey="3" title="Docs"> </Tab>
-                        <Tab eventKey="4" title="Photos"></Tab>
+                        <Tab eventKey="4" title="Notes"> </Tab>
                     </Tabs>
                 </Col>
             </div>
