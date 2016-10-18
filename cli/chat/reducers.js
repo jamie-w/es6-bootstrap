18ab16d96a24cc53d55d4c65dbbf7ff957289075
Jamie Weatherby <jamie.weatherby@gmail.com>
2016-10-18 10:18:51 -0400

    Re-enable text highlighting
    That's an odd thing to disable.

--- cli/chat/reducers.js
+++ cli/chat/reducers.js
@@ -5,7 +5,6 @@ var logger = bows("chat.reducers");
 export default (state=[], action) => {
     switch(action.type){
         case 'SEND_MSG':
-            logger(state, action);
             var chatIndex = state.findIndex(c => action.chat_uid === c.uid),
                 chats = [...state];
             chats[chatIndex].msgs.push(action.msg);
